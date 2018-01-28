480a6c6ac1b1bbb00ac46f667559df6263302479
xiaobaiHB <xiaobai_hb@qq.com>
2018-01-28 13:48:05 +0800

    update as 3.0 and add configure activity

---
+++ app/src/main/java/com/xiaobaihb/shirouface/FaceConfigAct.java
@@ -0,0 +1,18 @@
+package com.xiaobaihb.shirouface;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by xiaobaihb on 2018/1/27.
+ */
+
+public class FaceConfigAct extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_shirou_face_configure);
+    }
+}
